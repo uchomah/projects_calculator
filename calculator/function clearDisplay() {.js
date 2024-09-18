397bb147882c5e49c29c25781e1466133e3c66e5
uchomah <emmanuelu108@gmail.com>
2024-09-18 11:52:59 +0100

    Add files via upload

---
+++ calculator/function clearDisplay() {.js
@@ -0,0 +1,23 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function appendCharacter(character) {
+    let display = document.getElementById('display');
+    display.value += character;
+}
+
+function calculateResult() {
+    let display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        alert('Invalid expression');
+        display.value = '';
+    }
+}
